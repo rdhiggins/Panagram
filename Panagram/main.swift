cdfdf1eefc5cd3778caac4814657ef743ca331ac
Rodger Higgins <rdhiggins@gmail.com>
2016-05-31 16:27:10 -0500

    Initial Commit

---
+++ Panagram/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Panagram
+//
+//  Created by Rodger Higgins on 5/31/16.
+//  Copyright © 2016 Spazstik Software. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
